65fc94c02a1573410e508030b9cb561e3a121028
Murphy Huang <huangyhq@gmail.com>
2019-08-18 23:15:03 -0400

    Update rename_files_by_regex.sh

--- rename_files_by_regex.sh
+++ rename_files_by_regex.sh
@@ -6,3 +6,11 @@ for file in *.nii; do
     mv "$file" "$newname"
   fi
 done
+
+for file in *_conn.mat; do
+  if [ -e "$file" ]; then
+    newname=`echo "$file" | sed -E 's/^([0-9]{6}\_rfMRI\_REST[1|2]\_LR\_conn)\.mat/\1_mwcc.mat/'`
+    echo "mv $file $newname"
+    mv "$file" "$newname"
+  fi
+done
